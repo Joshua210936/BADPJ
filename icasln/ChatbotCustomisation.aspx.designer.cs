ff1df8e5db64705fb78918e7d4ec2f3c9b2ecbcb
Joshua210936 <93179779+Joshua210936@users.noreply.github.com>
2024-02-13 03:57:10 +0800

    chatbot ui done

--- icasln/ChatbotCustomisation.aspx.designer.cs
+++ icasln/ChatbotCustomisation.aspx.designer.cs
@@ -78,24 +78,6 @@ namespace icasln
         protected global::System.Web.UI.WebControls.Button Button2;
 
         /// <summary>
-        /// ChatbotList control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ChatbotList;
-
-        /// <summary>
-        /// Button4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button4;
-
-        /// <summary>
         /// ChatbotUpdateList control.
         /// </summary>
         /// <remarks>
@@ -112,5 +94,23 @@ namespace icasln
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button5;
+
+        /// <summary>
+        /// ChatbotList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ChatbotList;
+
+        /// <summary>
+        /// Button4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
     }
 }
